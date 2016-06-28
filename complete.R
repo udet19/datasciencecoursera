1de0bc246e240992aefbd088cebbbe04621f3834
udet19 <e.gilenko@operationalrisksolutions.com>
2016-06-28 11:42:39 +0200

    Cleaned function

--- complete.R
+++ complete.R
@@ -9,19 +9,15 @@ complete<-function(directory,id=1:332){
     my_file <- read.csv(file.path(directory,paste(formatC(counter,width=3,flag=0),".csv",sep="")), header = TRUE)
     
     #append to data frame
-    #my_data<-rbind(data.frame(my_file),my_data)
     my_data<-data.frame(my_file)
     
     #filter complete rows
     complete_data<-my_data[!is.na(my_data["sulfate"])&!is.na(my_data["nitrate"]),]["ID"]
     
+    #create output
     output<-rbind(output,c(counter,nrow(complete_data)))
     }
   }
-  
-  
-  #create output
-  #output<-as.data.frame(table(complete_data))
   
   #name columns
   names(output)<-c("id","nobs")
