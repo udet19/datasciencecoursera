210ae0f5c5b2d50ccd7beda52c729bf76f1e87cf
udet19 <e.gilenko@operationalrisksolutions.com>
2016-06-28 11:40:33 +0200

    Fixed complete function

--- complete.R
+++ complete.R
@@ -1,24 +1,30 @@
 complete<-function(directory,id=1:332){
   #define variables
   output<-data.frame()
-  my_data<-data.frame()
+
   
   for (counter in id){
     if (file.exists(file.path(directory,paste(formatC(counter,width=3,flag=0),".csv",sep="")))){ 
     #read file
     my_file <- read.csv(file.path(directory,paste(formatC(counter,width=3,flag=0),".csv",sep="")), header = TRUE)
+    
     #append to data frame
-    my_data<-rbind(data.frame(my_file),my_data)
+    #my_data<-rbind(data.frame(my_file),my_data)
+    my_data<-data.frame(my_file)
+    
+    #filter complete rows
+    complete_data<-my_data[!is.na(my_data["sulfate"])&!is.na(my_data["nitrate"]),]["ID"]
+    
+    output<-rbind(output,c(counter,nrow(complete_data)))
     }
   }
-  #filter complete rows
-  complete_data<-my_data[!is.na(my_data["sulfate"])&!is.na(my_data["nitrate"]),]["ID"]
+  
   
   #create output
-  output<-as.data.frame(table(complete_data))
+  #output<-as.data.frame(table(complete_data))
   
   #name columns
   names(output)<-c("id","nobs")
   
-  output[id,]
+  output
 }
